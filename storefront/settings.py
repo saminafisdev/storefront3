bec1a4d057e4c0536b73ee8020898ec6c02a93cf
Sami Nafis <78338492+sodiumfishh@users.noreply.github.com>
2024-04-17 23:48:27 +0600

    Configuring email backend

--- storefront/settings.py
+++ storefront/settings.py
@@ -171,3 +171,10 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("JWT",), "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
+
+EMAIL_BACKEND = "django.core.mail.backend.smtp.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = "from@samibuy.com"

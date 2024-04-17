35fb5f8bf408299fcf33c35d0afefd91bd9f3ae2
Sami Nafis <78338492+sodiumfishh@users.noreply.github.com>
2024-04-18 00:06:54 +0600

    Sending Templated Emails

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,14 @@
 from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
     try:
-        send_mail(
-            "subject",
-            "message",
-            "user@email.com",
-            ["bob@email.com"],
-            html_message="<h3 style='font-family: Arial;'>Hello there!</h3><p>Hey! <br> This is our first Email! So excited! ARE YOU??!!</p>",
+        message = BaseEmailMessage(
+            template_name="emails/hello.html", context={"name": "Bob"}
         )
+        message.send(["john@moshbuy.com"])
     except BadHeaderError:
         pass
 
